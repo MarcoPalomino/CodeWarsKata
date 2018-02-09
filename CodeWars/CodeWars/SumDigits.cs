c886e41021fdbd455a94d123de70749aac25cc58
marco.palomino <palomino.marco@pucp.edu.pe>
2018-02-09 12:20:37 -0500

    Add SumDigits Kata and Unit Tests

---
+++ CodeWars/CodeWars/SumDigits.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class SumDigits
+    {
+        public int SumDigitsInNumber(int number)
+        {
+            var isNegative = number < 0;
+            var digitsInNumber = new List<int>();
+            var absoluteValue = isNegative ? number * -1 : number;
+
+            while (absoluteValue != 0)
+            {
+                var integerPart = absoluteValue / (int)Math.Pow(10, absoluteValue.ToString().Length - 1);
+                absoluteValue = absoluteValue - integerPart * (int)Math.Pow(10, absoluteValue.ToString().Length - 1);
+
+                int elementToAdd;
+                if (integerPart > 0)
+                    elementToAdd = integerPart;
+                else
+                {
+                    elementToAdd = absoluteValue;
+                    absoluteValue = 0;
+                }
+
+                digitsInNumber.Add(elementToAdd);
+            }
+
+            return digitsInNumber.AsEnumerable().Sum();
+        }
+    }
+}
---
+++ CodeWars/CodeWarsTests/SumDigitsTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace CodeWars.Tests
+{
+    [TestClass()]
+    public class SumDigitsTests
+    {
+        [TestMethod()]
+        public void SumDigitsInNumberTest()
+        {
+            var number = 133;
+
+            var sumDigits = new SumDigits();
+
+            var result = sumDigits.SumDigitsInNumber(number);
+
+            Assert.AreEqual(7, result, "Incorrect");
+        }
+    }
+}
