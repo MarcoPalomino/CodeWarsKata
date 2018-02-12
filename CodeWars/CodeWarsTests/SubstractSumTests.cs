74dcb52de7cbb790c303f4e8df883476a52e69f4
marco.palomino <palomino.marco@pucp.edu.pe>
2018-02-12 10:30:00 -0500

    Add SubstractSum Kata and tests.

---
+++ CodeWars/CodeWarsTests/SubstractSumTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    [TestClass()]
+    public class SubstractSumTests
+    {
+        [TestMethod()]
+        public void SubstractTheSumTest()
+        {
+            var number = 1561964189;
+
+            var substractSum = new SubstractSum();
+
+            var result = substractSum.SubstractTheSum(number);
+
+            Assert.AreEqual("apple", result, "Incorrect");
+        }
+
+        [TestMethod()]
+        public void SubstractTheSumTestApple()
+        {
+            var number = 97;
+
+            var substractSum = new SubstractSum();
+
+            var result = substractSum.SubstractTheSum(number);
+
+            Assert.AreEqual("apple", result, "Incorrect");
+        }
+    }
+
+}
