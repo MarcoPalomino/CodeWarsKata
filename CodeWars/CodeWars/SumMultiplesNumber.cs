d033b109ae2d1e6a273f189b252f7ce2ea1641f9
marco.palomino <palomino.marco@pucp.edu.pe>
2018-02-12 10:37:11 -0500

    Add kata "Sum of all the multiples of 3 or 5" (7kyu)

---
+++ CodeWars/CodeWars/SumMultiplesNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class SumMultiplesNumber
+    {
+        /*
+         Your task is to write function findSum.
+         Upto and including n, this function will return the sum of all multiples of 3 and 5.
+         For example:
+         FindSum(5) should return 8 (3 + 5)
+         FindSum(10) should return 33 (3 + 5 + 6 + 9 + 10)    
+        */
+        public int FindSum(int number)
+        {
+            var multiplesToValidate = new List<int> { 3, 5 };
+            if (number < 0)
+                throw new Exception("Please, enter positive numbers");
+
+            var multiplesSelected = from num in Enumerable.Range(1, number)
+                where multiplesToValidate.Any(item => num % item == 0)
+                select num;
+
+            return multiplesSelected.Sum();
+        }
+    }
+}
---
+++ CodeWars/CodeWarsTests/SumMultiplesNumberTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    [TestClass()]
+    public class SumMultiplesNumberTests
+    {
+        [TestMethod()]
+        public void FindSumTest()
+        {
+            var number = 7;
+
+            var sumMultiplesNumber = new SumMultiplesNumber();
+
+            var result = sumMultiplesNumber.FindSum(number);
+
+            Assert.AreEqual(14, result, "Incorrect");
+        }
+
+        [TestMethod()]
+        public void FindSumTestForBigNumber()
+        {
+            var number = 10;
+
+            var sumMultiplesNumber = new SumMultiplesNumber();
+
+            var result = sumMultiplesNumber.FindSum(number);
+
+            Assert.AreEqual(33, result, "Incorrect");
+        }
+    }
+}
