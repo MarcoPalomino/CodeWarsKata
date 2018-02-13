7acc884e156307df6100b7edd8ffb4c8fca2fb25
marco.palomino <palomino.marco@pucp.edu.pe>
2018-02-13 14:42:38 -0500

    Add "Highest Scoring Word" kata (6 kyu)

---
+++ CodeWars/CodeWars/HighestScoringWord.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeWars
+{
+    public class HighestScoringWord
+    {
+        /*
+        Given a string of words, you need to find the highest scoring word.
+
+        Each letter of a word scores points according to it's position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+        You need to return the highest scoring word as a string.
+
+        If two words score the same, return the word that appears earliest in the original string.
+
+        All letters will be lowercase and all inputs will be valid. 
+        */
+        public string HighScoring(string words)
+        {
+            var separator = ' ';
+            var wordsToEvaluate = words.Split(separator).ToList();
+            var wordsWithAsciiSum = new Dictionary<string, int>();
+
+            wordsToEvaluate.ForEach(item =>
+            {
+                if(!wordsWithAsciiSum.ContainsKey(item))
+                    wordsWithAsciiSum.Add(item,Encoding.ASCII.GetBytes(item).Select(value => (int) value).Sum() - 96 * item.Length);
+            });
+            
+            return wordsWithAsciiSum.OrderByDescending(item=>item.Value).FirstOrDefault().Key;
+        }
+    }
+}
---
+++ CodeWars/CodeWarsTests/HighestScoringWordTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    [TestClass()]
+    public class HighestScoringWordTests
+    {
+        [TestMethod()]
+        public void HighScoringTest()
+        {
+            var words = "man i need a taxi up to ubud";
+
+            var highestScoringWord = new HighestScoringWord();
+            var result = highestScoringWord.HighScoring(words);
+
+            Assert.AreEqual("taxi", result, "Is incorrect");
+        }
+
+        [TestMethod()]
+        public void HighScoringWithMultipleValuesTest()
+        {
+            var words = "what time are we climbing up to volcano";
+
+            var highestScoringWord = new HighestScoringWord();
+            var result = highestScoringWord.HighScoring(words);
+
+            Assert.AreEqual("volcano", result, "Is incorrect");
+        }
+
+
+        [TestMethod()]
+        public void HighScoringWithEmptyValuesTest()
+        {
+            var words = "";
+
+            var highestScoringWord = new HighestScoringWord();
+            var result = highestScoringWord.HighScoring(words);
+
+            Assert.AreEqual("", result, "Is incorrect");
+        }
+
+        [TestMethod()]
+        public void TestsWithLongWords()
+        {
+            var words = "uhptokvq evextjlhmj";
+
+            var highestScoringWord = new HighestScoringWord();
+            var result = highestScoringWord.HighScoring(words);
+
+            Assert.AreEqual("uhptokvq", result, "Is incorrect");
+        }
+
+        [TestMethod()]
+        public void TestsWithSameWords()
+        {
+            var words = "house the house for working";
+
+            var highestScoringWord = new HighestScoringWord();
+            var result = highestScoringWord.HighScoring(words);
+
+            Assert.AreEqual("working", result, "Is incorrect");
+        }
+    }
+}
