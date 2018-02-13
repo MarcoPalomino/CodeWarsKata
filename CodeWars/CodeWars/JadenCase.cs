4ffd3af43fb4d946d68fcfc6feafd38070301098
marco.palomino <palomino.marco@pucp.edu.pe>
2018-02-13 15:23:18 -0500

    Add "Jaden Casing Strings" kata (7 kyu)

---
+++ CodeWars/CodeWars/JadenCase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public class JadenCase
+    {
+        /*
+        Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). 
+        Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word.
+        
+        Your task is to convert strings to how they would be written by Jaden Smith. 
+        The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them. 
+
+        Example:
+        Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+        Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+        */
+
+        public string ToJadenCase(string phrase)
+        {
+            var finalSentence = string.Empty;
+            phrase.Split(' ').ToList().ForEach(word =>
+            {
+                var remainingWord = word.ToLowerInvariant().Remove(0, 1);
+                finalSentence = finalSentence + " " + word.ToCharArray(0, word.Length).FirstOrDefault().ToString().ToUpperInvariant() + remainingWord;
+            });
+
+            return finalSentence.Trim();
+        }
+    }
+}
---
+++ CodeWars/CodeWarsTests/JadenCaseTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    [TestClass()]
+    public class JadenCaseTests
+    {
+        [TestMethod()]
+        public void ToJadenCaseTest()
+        {
+            var originalSentence = "How can mirrors be reAl if our eYes aren't real";
+
+            var jadenCase = new JadenCase();
+
+            var results = jadenCase.ToJadenCase(originalSentence);
+
+            Assert.AreEqual("How Can Mirrors Be Real If Our Eyes Aren't Real",results,"Strings didn't match.");
+        }
+    }
+}
