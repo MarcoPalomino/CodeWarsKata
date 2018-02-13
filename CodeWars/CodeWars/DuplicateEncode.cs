cd2adec14fb940a9060fa381daa13a3b852007c2
marco.palomino <palomino.marco@pucp.edu.pe>
2018-02-13 15:48:51 -0500

    Add "Duplicate Encoder" kata (6 kyu)

---
+++ CodeWars/CodeWars/DuplicateEncode.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+
+namespace CodeWars
+{
+    public class DuplicateEncode
+    {
+        /*
+         The goal of this exercise is to convert a string to a new string where each character in the new string is '(' if that character appears only once in the original string, or ')' if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+        Examples:
+
+        "din" => "((("
+
+        "recede" => "()()()"
+
+        "Success" => ")())())"
+
+        "(( @" => "))(("
+             
+        */
+        public string DuplicateEncoder(string wordToEncode)
+        {
+            var newWordEncode = string.Empty;
+            wordToEncode.ToCharArray().ToList().ForEach(letter=>
+            {
+                newWordEncode = newWordEncode + (wordToEncode.AsEnumerable()
+                                                             .Count(item => item.ToString().ToLower() == letter.ToString().ToLower()) > 1
+                                                             ? ")" : "(");
+            });
+
+            return newWordEncode;
+        }
+    }
+}
---
+++ CodeWars/CodeWarsTests/DuplicateEncodeTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    [TestClass()]
+    public class DuplicateEncodeTests
+    {
+        [TestMethod()]
+        public void SimpleDuplicateEncoderTest()
+        {
+            var wordToEncode = "dic";
+            var duplicateEncoder = new DuplicateEncode();
+
+            var result = duplicateEncoder.DuplicateEncoder(wordToEncode);
+
+            Assert.AreEqual("(((", result, "Incorrect");
+        }
+
+        [TestMethod()]
+        public void LongDuplicateEncoderTest()
+        {
+            var wordToEncode = "recede";
+            var duplicateEncoder = new DuplicateEncode();
+
+            var result = duplicateEncoder.DuplicateEncoder(wordToEncode);
+
+            Assert.AreEqual("()()()", result, "Incorrect");
+        }
+
+        [TestMethod()]
+        public void DuplicateEncoderTestWithCase()
+        {
+            var wordToEncode = "Success";
+            var duplicateEncoder = new DuplicateEncode();
+
+            var result = duplicateEncoder.DuplicateEncoder(wordToEncode);
+
+            Assert.AreEqual(")())())", result, "should ignore case");
+        }
+        
+    }
+}
