d57c0c6454eb685036862d19dcebaa5f5d9e8400
marco.palomino <palomino.marco@pucp.edu.pe>
2018-02-14 10:21:39 -0500

    Add first draft on "GapInPrimes" kata (5 kyu)

---
+++ CodeWars/CodeWarsTests/GapInPrimesTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsTests
+{
+    [TestClass()]
+    public class GapInPrimesTests
+    {
+        [TestMethod()]
+        public void DoGapTest()
+        {
+            var gap = 2;
+            var initialNumber = 100;
+            var finalNumber = 110;
+
+            var gapInPrimes = new GapInPrimes();
+            var result = gapInPrimes.DoGap(gap, initialNumber, finalNumber);
+
+            Assert.AreEqual(new long[] { 101, 103 }, result, "Incorrect");
+            
+        }
+    }
+}
