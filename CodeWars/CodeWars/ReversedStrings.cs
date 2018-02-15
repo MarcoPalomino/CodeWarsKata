26dde1a34a986288493f34baeac604a10731b4eb
marco.palomino <palomino.marco@pucp.edu.pe>
2018-02-15 08:25:47 -0500

    Add first draft on "ReversedStrings" kata (7 kyu)

---
+++ CodeWars/CodeWars/ReversedStrings.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class ReversedStrings
+{
+    public string DoReverse(string str)
+    {
+        throw new NotImplementedException("TODO: Kata.Solution(string) => string");
+    }
+}
