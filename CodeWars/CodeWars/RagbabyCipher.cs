07aa7244e2e41a529f06d77240b4bc53a6edc7c9
marco.palomino <palomino.marco@pucp.edu.pe>
2018-02-15 08:45:35 -0500

    Add first draft of "RagbabyCipher" kata (6 kyu)

---
+++ CodeWars/CodeWars/RagbabyCipher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class RagbabyCipher
+    {
+        public string Encode(string text, string key)
+        {
+            return text;
+        }
+
+        public string Decode(string text, string key)
+        {
+            return text;
+        }
+    }
+}
---
+++ CodeWars/CodeWarsTests/RagbabyCipherTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsTests
+{
+    [TestClass()]
+    public class RagbabyCipherTests
+    {
+        [TestMethod()]
+        public void EncodeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DecodeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
