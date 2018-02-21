9e4bd432ab8e19bb0a0d8807649c6ac7190d6633
marco.palomino <palomino.marco@pucp.edu.pe>
2018-02-21 14:26:29 -0500

    Add AlphabetSymmetry kata (7 kyu)

---
+++ CodeWars/CodeWars/AlphabetSymmetry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class AlphabetSymmetry
+    {
+        public List<int> SolveSymmetry(List<string> arr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
