44b6760a113d1a0dd69355724a301b5b9467b01d
marco.palomino <palomino.marco@pucp.edu.pe>
2018-02-27 17:05:11 -0500

    Modify IQTestEvenness

--- CodeWars/CodeWars/IQTestEvenness.cs
+++ CodeWars/CodeWars/IQTestEvenness.cs
@@ -8,11 +8,11 @@ namespace CodeWars
         public int TestEveness(string numbers)
         {
             var numbersToEvaluate = numbers.Split(' ').ToList();
-            var evenNumbers = numbersToEvaluate.Count(number => (Int32.Parse(number)) % 2 == 0) == 1;
+            var evenNumbers = numbersToEvaluate.Count(number => (int.Parse(number)) % 2 == 0) == 1;
             
             var indexNumber = numbersToEvaluate.IndexOf(evenNumbers 
-                ? numbersToEvaluate.FirstOrDefault(number => (Int32.Parse(number)) % 2 == 0) 
-                : numbersToEvaluate.FirstOrDefault(number => (Int32.Parse(number)) % 2 != 0));
+                ? numbersToEvaluate.FirstOrDefault(number => (int.Parse(number)) % 2 == 0) 
+                : numbersToEvaluate.FirstOrDefault(number => (int.Parse(number)) % 2 != 0));
 
             return indexNumber + 1;
         }
