37b9b8d34dcc35f03f74c3383bfe46c1529bb96a
marco.palomino <palomino.marco@pucp.edu.pe>
2018-03-06 09:27:34 -0500

    Add "Folding your way to the moon" kata (7 kyu)

---
+++ CodeWars/CodeWarsTests/FoldingWayToTheMoonTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+
+
+namespace CodeWarsTests
+{
+    [TestClass()]
+    public class FoldingWayToTheMoonTests
+    {
+        [TestMethod()]
+        public void FoldToTest()
+        {
+            var distance = 384000000;
+            var expected = 42;
+            var foldWayToMoon = new FoldingWayToTheMoon();
+            var results = foldWayToMoon.FoldTo(distance);
+            
+            Assert.AreEqual(expected, results);
+        }
+    }
+}
