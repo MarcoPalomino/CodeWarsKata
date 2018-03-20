730344fc5b7c3d86bc51dcc099ec2885270e5a72
marco.palomino <palomino.marco@pucp.edu.pe>
2018-03-20 07:23:32 -0500

    Add first version of "Credit Card Number validation" kata (6 kyu)

---
+++ CodeWars/CodeWars/CreditCardValidation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class CreditCardValidation
+    {
+        public bool Dovalidate(string n)
+        {
+            return false;
+        }
+    }
+}
---
+++ CodeWars/CodeWarsTests/CreditCardValidationTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsTests
+{
+    [TestClass()]
+    public class CreditCardValidationTests
+    {
+        [TestMethod()]
+        public void DovalidateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
