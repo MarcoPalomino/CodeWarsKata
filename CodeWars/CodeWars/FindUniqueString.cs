1a410dcf71f577b8b7a5085b9fb3c5a331160d0e
marco.palomino <palomino.marco@pucp.edu.pe>
2018-03-23 12:30:08 -0500

    Add "FindUniqueString" kata (5 kyu)

---
+++ CodeWars/CodeWars/FindUniqueString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class FindUniqueString
+    {
+        /*
+        
+        There is an array of strings. All strings contains similar letters except one. Try to find it!
+
+        findUniq([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a' ]) === 'BbBb'
+        findUniq([ 'abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba' ]) === 'foo'
+        Strings may contain spaces. Spaces is not significant, only non-spaces symbols matters. E.g. string that contains only spaces is like empty string.
+
+        It’s guaranteed that array contains more than 3 strings.
+ 
+        */
+
+        public string FindUnique(string[] words)
+        {
+            return string.Empty;
+        }
+
+    }
+}
---
+++ CodeWars/CodeWarsTests/FindUniqueStringTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsTests
+{
+    [TestClass()]
+    public class FindUniqueStringTests
+    {
+        [TestMethod()]
+        public void FindUniqueTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
