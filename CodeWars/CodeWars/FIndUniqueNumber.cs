bc0374d0b52ca1c4ba41571bd47ef4fcb1f1c395
marco.palomino <palomino.marco@pucp.edu.pe>
2018-03-23 11:45:32 -0500

    Add FindUnique kata (6 kyu)

---
+++ CodeWars/CodeWars/FIndUniqueNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class FindUniqueNumber
+    {
+        /*
+         Caution: This kata does not currently have any known supported versions for C#. It may not be completable due to dependencies on out-dated libraries/language versions.
+         There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+        findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+        findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+        It’s guaranteed that array contains more than 3 numbers.
+
+        The tests contain some very huge arrays, so think about performance.
+
+        This is the first kata in series:
+
+        Find the unique number (this kata)
+        Find the unique string
+        Find The Unique
+         
+        */
+
+        public int GetUnique(IEnumerable<int> numbers)
+        {
+            return numbers.GroupBy(item => item).FirstOrDefault(item=>item.Count()==1).Key;
+        }
+
+
+
+
+
+    }
+}
---
+++ CodeWars/CodeWarsTests/FIndUniqueNumberTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsTests
+{
+    [TestClass()]
+    public class FindUniqueNumberTests
+    {
+        [TestMethod()]
+        public void GetUniqueTest()
+        {
+
+            var collectionTest = new[] { -2, 2, 2, 2 };
+            var findUniqueNumber = new FindUniqueNumber();
+
+            var result = findUniqueNumber.GetUnique(collectionTest);
+
+            Assert.AreEqual(-2, result, "Incorrect");
+        }
+    }
+}
