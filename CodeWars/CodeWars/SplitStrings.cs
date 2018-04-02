433883d35d9a9facbca8a6a20e7e4eda268ede11
marco.palomino <palomino.marco@pucp.edu.pe>
2018-04-02 16:58:15 -0500

    Add final solution to "Split Strings" kata (6 kyu)

--- CodeWars/CodeWars/SplitStrings.cs
+++ CodeWars/CodeWars/SplitStrings.cs
@@ -17,19 +17,15 @@ namespace CodeWars
             {
                 var lastLetter = word[word.Length - 1];
                 word = word.Remove(word.Length - 1, 1);
-                for (int i = 0; i < word.Length - 1; i += 2)
-                {
-                    splitStrings.Add(word[i].ToString() + word[i + 1].ToString());
-                }
-
-                splitStrings.Add(lastLetter.ToString() + "_");
+                for (int i = 0; i <= word.Length - 2; i += 2)
+                    splitStrings.Add(word[i].ToString() + word[i + 1]);
+                
+                splitStrings.Add(lastLetter + "_");
             }
             else
             {
                 for (int i = 0; i <= word.Length - 2; i += 2)
-                {
-                    splitStrings.Add(word[i].ToString() + word[i + 1].ToString());
-                }
+                    splitStrings.Add(word[i].ToString() + word[i + 1]);
             }
             return splitStrings.ToArray();
         }
