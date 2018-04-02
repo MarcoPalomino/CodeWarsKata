1970d5c4e6933a6b6d9220ce56e8df24a384a826
marco.palomino <palomino.marco@pucp.edu.pe>
2018-04-02 16:24:03 -0500

    Add draft on "Split Strings" kata (6 kyu)

---
+++ CodeWars/CodeWarsTests/SplitStringsTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsTests
+{
+    [TestClass()]
+    public class SplitStringsTests
+    {
+        [TestMethod()]
+        public void SolutionTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
